5e5f6dd5a41b8f4bb20f8bb31b994d6b20ceee3f
Isuru sankhajith <isankhajith@gmail.com>
2023-11-04 20:03:33 +0530

    Tasks_updated

--- Java_fundamentals/Tasks.java
+++ Java_fundamentals/Tasks.java
@@ -29,20 +29,36 @@ public class Tasks{
 	}
 }
 
-*/
+
 
 /////////////////////////////////////////////////////////////////////////////
-
+import java.util.Scanner;
 public class Tasks{
 	public static void main(String []args){
-		System.out.println(5 + (15/2) -8* 2 % 3);
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter number  :");
+		String userinput = scanner.nextLine();
+		
+		System.out.println("You entered  :" + userinput);
+		
+		scanner.close();
 
 	}
 }
 
+*/
+/////////////////////////////////////////////////////////////////////////////
 
-
-
+import java.util.Scanner;
+public class Tasks{
+	public static void main(String [] args){
+		Scanner x = new Scanner (System.in);
+		System.out.print("Enter number  :");
+		String userinput = x.nextLine();
+		System.out.print("You Entered  ::" + userinput);
+		
+	}
+}
 
 
 
