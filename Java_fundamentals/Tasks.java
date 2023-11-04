29ac10b0e32f978ca929ee4e17f5ab147f0386d2
Isuru sankhajith <isankhajith@gmail.com>
2023-11-04 19:34:45 +0530

    Tasks_updated

---
+++ Java_fundamentals/Tasks.java
@@ -0,0 +1,52 @@
+/*
+public class Example1{
+	public static void main(String args []){
+		System.out.println("Hello World");
+	}
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+
+public class Tasks{
+	public static void main(String []args){
+		System.out.println( 2 + 3 );
+		System.out.println( 2 / 3 );
+		System.out.println( 2 % 3 );
+	}
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+
+public class Tasks{
+	public static void main(String []args){
+		System.out.println( -5 + 8 * 6 );
+		System.out.println((55-9) % 9);
+		System.out.println (20 + (-3*5)/8);
+		System.out.println(5 + (15/2) -8* 2 % 3);
+
+	}
+}
+
+*/
+
+/////////////////////////////////////////////////////////////////////////////
+
+public class Tasks{
+	public static void main(String []args){
+		System.out.println(5 + (15/2) -8* 2 % 3);
+
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
