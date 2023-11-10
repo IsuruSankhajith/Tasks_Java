b0f57dcf1933519cd591668a55b0843bc80c6ca6
Isuru sankhajith <isankhajith@gmail.com>
2023-11-10 11:16:18 +0530

    Modified code

--- Java_fundamentals/Tasks.java
+++ Java_fundamentals/Tasks.java
@@ -787,32 +787,26 @@ public class Tasks {
 
 import java.util.Scanner;
 
-public class Tasks{
-	public static void main(String args[]){
-	
-	int dec_num, rem;
-	
-	String hexdec_num= "";
-	
-	char hex[] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-	
-	Scanner in = new Scanner(System.in);
-	
-	System.out.print("Input decima number : ");
-	dec_num = in.nextInt();
-	
-	while (dec_num > 0){
-		rem = dec_num % 16;
-		hexdec_num = hex[rem] + hexdec_num;
-		dec_num = dec_num / 16; 
-	
-	}
-	System.out.print("Hexadecimal number is :" + hexdec_num + "\n");
-	
-	
-	
-	}	
+public class Tasks {
+    public static void main(String args[]) {
+        int dec_num, rem;
+        String hexdec_num = "";
+        char hex[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Input decimal number: ");
+        dec_num = in.nextInt();
+
+        while (dec_num > 0) {
+            rem = dec_num % 16;
+            hexdec_num = hex[rem] + hexdec_num;
+            dec_num = dec_num / 16;
+        }
+
+        System.out.print("Hexadecimal number is: " + hexdec_num + "\n");
+    }
 }
+
 
 
 
