58da0e62c10e7c5c575c7162fb8b3cd1eb2c2c94
Isuru sankhajith <isankhajith@gmail.com>
2023-11-14 14:29:57 +0530

    Added leetcode 32

--- Java_fundamentals/Tasks.java
+++ Java_fundamentals/Tasks.java
@@ -1102,6 +1102,21 @@ class DigitSumCalculator {
         return sum;
     }
 }
+public class MyFriends {
+  public static void main(String args[]) {
+
+    AllMyfriends contacts = new AllMyfriends();
+
+    contacts.add("John","Smith","johnsmith@ecu.edu.au");
+    contacts.add("Felix","Finley","felixf@uwa.edu.au");
+    contacts.add("Mike","Spelling","mikespe@uow.edu.au");
+    contacts.add("Kathy","Ellis","kateellis@usydney.edu.au");
+    contacts.add("Debbie","Brown","Dbrowny@umelb.edu.au");
+
+    System.out.println(contacts.toString());
+      }
+}
+
 */
 //32///////////////////////////////////////////////////////////////////////////
 import java.util.Scanner;
@@ -1143,6 +1158,7 @@ class Hexagon {
 }
 
 
+//32///////////////////////////////////////////////////////////////////////////
 
 
 
