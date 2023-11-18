6d890510e41901480345f2ee59dc8f8a5caa6e03
Isuru sankhajith <isankhajith@gmail.com>
2023-11-18 09:51:25 +0530

    Modified code

---
+++ Java_fundamentals/Tasks2.java
@@ -0,0 +1,51 @@
+///////////////////////////////////////////////////////////////////////////
+/*
+public class Tasks2{
+	public static void main (String args[]){
+		System.out.println("sadfsdf");
+	}
+}
+
+
+
+public class Tasks2{
+	public static void main (String args[]){
+		System.out.println("Hello java");
+		System.out.println("Hello java");
+		System.out.println("Hello java");
+		System.out.println("Hello java");
+		System.out.println("Hello java");
+		System.out.println("Hello java");
+	}
+}
+///////////////////////////////////////////////////////////////////////////
+public class Tasks2{
+	public static void main (String args[]){
+		System.out.println("A");
+		System.out.print("B");
+		System.out.println("C");
+		System.out.print("D");
+	}
+}
+
+*/
+///////////////////////////////////////////////////////////////////////////
+
+public class Tasks2{
+	public static void main (String args[]){
+		System.out.println("1");
+		System.out.println("1000");
+		System.out.println("1.23");
+
+	}
+}
+
+
+
+
+
+
+
+
+
+
