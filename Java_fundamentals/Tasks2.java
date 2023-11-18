25dbd8319a634d411e32fcd5372c470a7b180d4c
Isuru sankhajith <isankhajith@gmail.com>
2023-11-18 15:01:19 +0530

    Modified code

--- Java_fundamentals/Tasks2.java
+++ Java_fundamentals/Tasks2.java
@@ -3,35 +3,39 @@
 
 */
 
-public class Tasks2{
-	public static void main(String args[]){
-		double A, p, r;
-		
-		p = 1000;
-		r = 0.5;
-		
-		A = p*(1+r)*p*(1+r);
-		
-		System.out.println(A);
-		
-	}
-}
 
-public class CompoundInterest {
+
+
+public class Tasks2 {
     public static void main(String[] args) {
-        // Input values
-        double principal = 1000; // original amount invested
-        double rate = 0.05;     // annual interest rate (5%)
-        int years = 5;          // number of years
+        // Check if two command-line arguments are provided
+        if (args.length < 2) {
+            System.out.println("Please provide two integers as command-line arguments.");
+            return;
+        }
 
-        // Calculate the amount using the compound interest formula
-        double amount = principal * Math.pow(1 + rate, years);
+        // Parse the command-line arguments as integers
+        try {
+            int number1 = Integer.parseInt(args[0]);
+            int number2 = Integer.parseInt(args[1]);
 
-        // Print the result
-        System.out.println("Total amount after " + years + " years: $" + amount);
+            // Calculate distance using Math.min() and Math.max()
+            int distance1 = Math.max(number1, number2) - Math.min(number1, number2);
+
+            // Calculate distance using Math.abs()
+            int distance2 = Math.abs(number1 - number2);
+
+            // Output the results
+            System.out.println("Using Math.min() and Math.max():");
+            System.out.println("Distance: " + distance1);
+
+            System.out.println("\nUsing Math.abs():");
+            System.out.println("Distance: " + distance2);
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid input. Please provide valid integers.");
+        }
     }
 }
-
 
 
 
--- Software_Testing/Week_01/Workshop1.java
+++ Software_Testing/Week_01/Workshop1.java
@@ -57,3 +57,39 @@ public class CompoundInterest {
         System.out.println("Total amount after " + years + " years: $" + amount);
     }
 }
+
+03
+//////////////////////////////////////////////////////////////////////////
+
+public class Tasks2 {
+    public static void main(String[] args) {
+        // Check if two command-line arguments are provided
+        if (args.length < 2) {
+            System.out.println("Please provide two integers as command-line arguments.");
+            return;
+        }
+
+        // Parse the command-line arguments as integers
+        try {
+            int number1 = Integer.parseInt(args[0]);
+            int number2 = Integer.parseInt(args[1]);
+
+            // Calculate distance using Math.min() and Math.max()
+            int distance1 = Math.max(number1, number2) - Math.min(number1, number2);
+
+            // Calculate distance using Math.abs()
+            int distance2 = Math.abs(number1 - number2);
+
+            // Output the results
+            System.out.println("Using Math.min() and Math.max():");
+            System.out.println("Distance: " + distance1);
+
+            System.out.println("\nUsing Math.abs():");
+            System.out.println("Distance: " + distance2);
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid input. Please provide valid integers.");
+        }
+    }
+}
+
+
