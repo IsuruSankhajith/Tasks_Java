8060d8fd960d5c5b0fa882ee27a26dcb2b5daf27
Isuru sankhajith <isankhajith@gmail.com>
2023-11-18 14:45:00 +0530

    Modified code

--- Java_fundamentals/Tasks2.java
+++ Java_fundamentals/Tasks2.java
@@ -3,30 +3,34 @@
 
 */
 
-public class Tasks2 {
-    public static void main(String[] args) {
-        // Check if two command-line arguments are provided
-        if (args.length < 2) {
-            System.out.println("Please provide two floating-point numbers as command-line arguments.");
-            return;
-        }
-
-        // Parse the command-line arguments as floating-point numbers
-        try {
-            double number1 = Double.parseDouble(args[0]);
-            double number2 = Double.parseDouble(args[1]);
-
-            // Compare the two numbers
-            double largerNumber = Math.max(number1, number2);
-
-            // Output the result
-            System.out.println("The larger number is: " + largerNumber);
-        } catch (NumberFormatException e) {
-            System.out.println("Invalid input. Please provide valid floating-point numbers.");
-        }
-    }
+public class Tasks2{
+	public static void main(String args[]){
+		double A, p, r;
+		
+		p = 1000;
+		r = 0.5;
+		
+		A = p*(1+r)*p*(1+r);
+		
+		System.out.println(A);
+		
+	}
 }
 
+public class CompoundInterest {
+    public static void main(String[] args) {
+        // Input values
+        double principal = 1000; // original amount invested
+        double rate = 0.05;     // annual interest rate (5%)
+        int years = 5;          // number of years
+
+        // Calculate the amount using the compound interest formula
+        double amount = principal * Math.pow(1 + rate, years);
+
+        // Print the result
+        System.out.println("Total amount after " + years + " years: $" + amount);
+    }
+}
 
 
 
--- Software_Testing/Week_01/Workshop1.java
+++ Software_Testing/Week_01/Workshop1.java
@@ -23,3 +23,37 @@ public class Tasks2 {
         }
     }
 }
+
+//////////////////////////////////////////////////////////////////////
+
+02)
+
+
+public class Tasks2{
+	public static void main(String args[]){
+		double A, p, r;
+		
+		p = 1000;
+		r = 0.5;
+		
+		A = p*(1+r)*p*(1+r);
+		
+		System.out.println(A);
+		
+	}
+}
+
+public class CompoundInterest {
+    public static void main(String[] args) {
+        // Input values
+        double principal = 1000; // original amount invested
+        double rate = 0.05;     // annual interest rate (5%)
+        int years = 5;          // number of years
+
+        // Calculate the amount using the compound interest formula
+        double amount = principal * Math.pow(1 + rate, years);
+
+        // Print the result
+        System.out.println("Total amount after " + years + " years: $" + amount);
+    }
+}
