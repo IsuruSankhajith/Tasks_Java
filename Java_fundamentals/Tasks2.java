d548f8081d667259f74abf2fb6712c04b93792bc
Isuru sankhajith <isankhajith@gmail.com>
2023-11-18 10:06:16 +0530

    Modified code

--- Java_fundamentals/Tasks2.java
+++ Java_fundamentals/Tasks2.java
@@ -28,7 +28,7 @@ public class Tasks2{
 	}
 }
 
-*/
+
 ///////////////////////////////////////////////////////////////////////////
 
 public class Tasks2{
@@ -39,6 +39,166 @@ public class Tasks2{
 
 	}
 }
+
+///////////////////////////////////////////////////////////////////////////
+public class Tasks2{
+	public static void main (String args[]){
+		System.out.println("A");
+		System.out.println(12324);
+		System.out.println(-1234);
+		System.out.println(1.2345);
+		System.out.println(0.12345);
+		System.out.println(-0.122344);
+		System.out.println('A');
+		System.out.println('6');
+		System.out.println(true);
+		System.out.println(false);
+		
+		
+	}
+}
+
+
+///////////////////////////////////////////////////////////////////////////
+
+
+public class Tasks2{
+	public static void main (String args[]){
+		System.out.print("A");
+		System.out.print("B");
+		System.out.print("C");
+		System.out.print("D");
+		System.out.print("E");		
+	}
+}
+
+
+
+public class Tasks2{
+	public static void main (String args[]){
+		System.out.print("A");
+		System.out.print("B");
+		System.out.print("");
+		System.out.print("D");
+		System.out.print("E");		
+	}
+}
+public class Tasks2{
+	public static void main (String args[]){
+		System.out.println("sadfsdf");
+	}
+}
+
+
+
+public class Tasks2{
+	public static void main (String args[]){
+		System.out.println("Hello java");
+		System.out.println("Hello java");
+		System.out.println("Hello java");
+		System.out.println("Hello java");
+		System.out.println("Hello java");
+		System.out.println("Hello java");
+	}
+}
+///////////////////////////////////////////////////////////////////////////
+public class Tasks2{
+	public static void main (String args[]){
+		System.out.println("A");
+		System.out.print("B");
+		System.out.println("C");
+		System.out.print("D");
+	}
+}
+
+
+///////////////////////////////////////////////////////////////////////////
+
+public class Tasks2{
+	public static void main (String args[]){
+		System.out.println("1");
+		System.out.println("1000");
+		System.out.println("1.23");
+
+	}
+}
+
+///////////////////////////////////////////////////////////////////////////
+public class Tasks2{
+	public static void main (String args[]){
+		System.out.println("A");
+		System.out.println(12324);
+		System.out.println(-1234);
+		System.out.println(1.2345);
+		System.out.println(0.12345);
+		System.out.println(-0.122344);
+		System.out.println('A');
+		System.out.println('6');
+		System.out.println(true);
+		System.out.println(false);
+		
+		
+	}
+}
+
+
+///////////////////////////////////////////////////////////////////////////
+
+
+public class Tasks2{
+	public static void main (String args[]){
+		System.out.print("A");
+		System.out.print("B");
+		System.out.print("C");
+		System.out.print("D");
+		System.out.print("E");		
+	}
+}
+
+
+///////////////////////////////////////////////////////////////////////////
+
+public class Tasks2{
+	public static void main (String args[]){
+		System.out.print("A");
+		System.out.print("B");
+		System.out.print("");
+		System.out.print("D");
+		System.out.print("E");		
+	}
+}
+
+*/
+///////////////////////////////////////////////////////////////////////////
+
+public class Tasks2{
+	public static void main(String args []){
+		int x;
+		x = 100;
+		System.out.println(x);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
