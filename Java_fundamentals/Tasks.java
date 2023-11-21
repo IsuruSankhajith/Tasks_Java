830c900af0b77073aea245391c37a22eeff6efa3
Isuru sankhajith <isankhajith@gmail.com>
2023-11-21 14:05:20 +0530

    Leetcode 52

--- Java_fundamentals/Tasks.java
+++ Java_fundamentals/Tasks.java
@@ -1765,7 +1765,22 @@ public class Tasks2{
 }
 
 
+import java.util.Scanner;
 
+class Tasks2 {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        String x = scanner.nextLine();
+        int num1 = Integer.parseInt(x);  // Corrected method name to parseInt
+
+        if (num1 > 0) {  // Corrected the condition to check if num1 is greater than 0
+            System.out.println("Positive number");
+        } else {
+            System.out.println("Not a positive number");
+        }
+    }
+}
 
 
 
--- Java_fundamentals/Tasks2.java
+++ Java_fundamentals/Tasks2.java
@@ -1,114 +1,18 @@
-///////////////////////////////////////////////////////////////////////////
-/*
 
-*/
+import java.util.Scanner;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+class Tasks2 {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        String x = scanner.nextLine();
+        int num1 = Integer.parseInt(x);  // Corrected method name to parseInt
 
-class Solution {
-    public List<List<String>> solveNQueens(int n) {
-        List<List<String>> result = new ArrayList<>();
-        char[][] board = new char[n][n];
-        for (char[] row : board) {
-            Arrays.fill(row, '.');
+        if (num1 > 0) {  // Corrected the condition to check if num1 is greater than 0
+            System.out.println("Positive number");
+        } else {
+            System.out.println("Not a positive number");
         }
-        solveNQueensUtil(board, 0, result);
-        return result;
-    }
-
-    private void solveNQueensUtil(char[][] board, int row, List<List<String>> result) {
-        if (row == board.length) {
-            result.add(constructSolution(board));
-            return;
-        }
-
-        for (int col = 0; col < board.length; col++) {
-            if (isValid(board, row, col)) {
-                board[row][col] = 'Q';
-                solveNQueensUtil(board, row + 1, result);
-                board[row][col] = '.'; // backtrack
-            }
-        }
-    }
-
-    private boolean isValid(char[][] board, int row, int col) {
-        // Check if there is a queen in the same column
-        for (int i = 0; i < row; i++) {
-            if (board[i][col] == 'Q') {
-                return false;
-            }
-        }
-
-        // Check if there is a queen in the upper-left diagonal
-        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
-            if (board[i][j] == 'Q') {
-                return false;
-            }
-        }
-
-        // Check if there is a queen in the upper-right diagonal
-        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
-            if (board[i][j] == 'Q') {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    private List<String> constructSolution(char[][] board) {
-        List<String> solution = new ArrayList<>();
-        for (char[] row : board) {
-            solution.add(new String(row));
-        }
-        return solution;
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
