b6d8ce292bd519aadf616f43e75dde0e63d13a92
Isuru sankhajith <isankhajith@gmail.com>
2023-11-21 21:59:38 +0530

    Modified code

--- Java_fundamentals/Tasks.java
+++ Java_fundamentals/Tasks.java
@@ -1834,22 +1834,22 @@ class Tasks2 {
         String z = scanner.nextLine();
         int num3 = Integer.parseInt(z);
         
-        if (num1 > num2)
-			if (num1 > num3)
-			
-			System.out.println("Higest Value number is " + num1);
-		if (num2 > num1)
-			if (num2 > num3)
-			
-			System.out.println("Higest Value number is " + num2);
-		
-		if (num3 > num1)
-			if (num3 > num2)
-			
-			System.out.println("Higest Value number is " + num3);
-
+        // Check which number is the highest among num1, num2, and num3
+        if (num1 > num2 && num1 > num3) {
+            System.out.println("Highest Value number is " + num1);
+        }
+        else if (num2 > num1 && num2 > num3) {
+            System.out.println("Highest Value number is " + num2);
+        }
+        else if (num3 > num1 && num3 > num2) {
+            System.out.println("Highest Value number is " + num3);
+        }
+        else {
+            System.out.println("Numbers are not distinct.");
+        }
     }
 }
+
 
 
 
