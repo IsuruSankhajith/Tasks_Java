275475c7c55e6001708c7e62a453d621d9a7d13f
Isuru sankhajith <isankhajith@gmail.com>
2023-11-21 17:46:20 +0530

    Leetcode 52

--- Java_fundamentals/Tasks.java
+++ Java_fundamentals/Tasks.java
@@ -1784,6 +1784,36 @@ class Tasks2 {
 
 
 
+import java.util.Scanner;
+
+class Tasks2 {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter first number: ");
+        String x = scanner.nextLine();
+        int num1 = Integer.parseInt(x);
+        
+        System.out.print("Enter second number: ");
+        String y = scanner.nextLine();
+        int num2 = Integer.parseInt(y);
+        
+        System.out.print("Enter third number: ");
+        String z = scanner.nextLine();
+        int num3 = Integer.parseInt(z);
+        
+        if(num1 < 10){
+			System.out.println("Higest number");
+		}else if(num2 < 50){
+			System.out.println("Hige adas");
+		}else if(num2 > 100){
+			System.out.println("Hige");
+		}
+    }
+}
+
+
+
 
 
 
--- Java_fundamentals/Tasks2.java
+++ Java_fundamentals/Tasks2.java
@@ -4,15 +4,27 @@ import java.util.Scanner;
 class Tasks2 {
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter number: ");
+        
+        System.out.print("Enter first number: ");
         String x = scanner.nextLine();
-        int num1 = Integer.parseInt(x);  // Corrected method name to parseInt
-
-        if (num1 > 0) {  // Corrected the condition to check if num1 is greater than 0
-            System.out.println("Positive number");
-        } else {
-            System.out.println("Not a positive number");
-        }
+        int num1 = Integer.parseInt(x);
+        
+        System.out.print("Enter second number: ");
+        String y = scanner.nextLine();
+        int num2 = Integer.parseInt(y);
+        
+        System.out.print("Enter third number: ");
+        String z = scanner.nextLine();
+        int num3 = Integer.parseInt(z);
+        
+        if(num1 < 10){
+			System.out.println("Higest number");
+		}else if(num2 < 50){
+			System.out.println("Hige adas");
+		}else if(num2 > 100){
+			System.out.println("Hige");
+		}
     }
 }
+
 
