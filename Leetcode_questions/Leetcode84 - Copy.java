eab1467b6285c0a1431b577f384d8559266effe5
Isuru sankhajith <isankhajith@gmail.com>
2023-11-28 23:56:37 +0530

    modified code

---
+++ Leetcode_questions/Leetcode84 - Copy.java
@@ -0,0 +1,93 @@
+
+/*
+Given an array of integers heights representing the histogram's bar height where the width of each bar is 1, return the area of the largest rectangle in the histogram.
+
+ 
+
+Example 1:
+
+
+Input: heights = [2,1,5,6,2,3]
+Output: 10
+Explanation: The above is a histogram where width of each bar is 1.
+The largest rectangle is shown in the red area, which has an area = 10 units.
+Example 2:
+
+
+Input: heights = [2,4]
+Output: 4
+ 
+
+Constraints:
+
+1 <= heights.length <= 105
+0 <= heights[i] <= 104
+*/
+class Solution {
+    public int maximalRectangle(char[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return 0;
+        }
+
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        int[] heights = new int[cols];
+        int maxRectangleArea = 0;
+
+        for (int i = 0; i < rows; i++) {
+            // Update the heights array for the current row
+            for (int j = 0; j < cols; j++) {
+                if (matrix[i][j] == '1') {
+                    heights[j] += 1;
+                } else {
+                    heights[j] = 0;
+                }
+            }
+
+            // Calculate the largest rectangle in histogram for the current row
+            maxRectangleArea = Math.max(maxRectangleArea, largestRectangleArea(heights));
+        }
+
+        return maxRectangleArea;
+    }
+
+    private int largestRectangleArea(int[] heights) {
+        int maxArea = 0;
+        int n = heights.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+
+        Stack<Integer> stack = new Stack<>();
+
+        // Calculate left boundaries for each bar
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && heights[stack.peek()] >= heights[i]) {
+                stack.pop();
+            }
+            left[i] = stack.isEmpty() ? -1 : stack.peek();
+            stack.push(i);
+        }
+
+        // Clear the stack for calculating right boundaries
+        stack.clear();
+
+        // Calculate right boundaries for each bar
+        for (int i = n - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && heights[stack.peek()] >= heights[i]) {
+                stack.pop();
+            }
+            right[i] = stack.isEmpty() ? n : stack.peek();
+            stack.push(i);
+        }
+
+        // Calculate the area for each bar and update the maxArea
+        for (int i = 0; i < n; i++) {
+            int width = right[i] - left[i] - 1;
+            int area = heights[i] * width;
+            maxArea = Math.max(maxArea, area);
+        }
+
+        return maxArea;
+    }
+}
