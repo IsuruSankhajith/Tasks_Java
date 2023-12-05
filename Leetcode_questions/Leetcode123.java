a1a87ced0e97723736353090e3421857c948001d
Isuru sankhajith <isankhajith@gmail.com>
2023-12-05 20:44:42 +0530

    modified code

--- Leetcode_questions/Leetcode123.java
+++ Leetcode_questions/Leetcode123.java
@@ -39,12 +39,15 @@ Constraints:
 */
 class Solution {
     public int maxProfit(int[] prices) {
+        // Check if the array has only one or zero elements
         int n = prices.length;
         if (n <= 1) {
-            return 0;
+            return 0; // No transactions possible, so profit is 0
         }
 
+        // Arrays to store maximum profit if selling on or before the current day
         int[] leftProfit = new int[n];
+        // Array to store maximum profit if buying on or after the current day
         int[] rightProfit = new int[n];
 
         // Calculate maximum profit if selling on or before the current day
@@ -61,6 +64,7 @@ class Solution {
             rightProfit[i] = Math.max(rightProfit[i + 1], maxPrice - prices[i]);
         }
 
+        // Calculate maximum total profit by combining left and right side profits
         int maxProfit = 0;
         for (int i = 0; i < n; i++) {
             maxProfit = Math.max(maxProfit, leftProfit[i] + rightProfit[i]);
