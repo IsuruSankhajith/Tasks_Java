0669569809875c1c3ea5e77de8d095398dad490d
Isuru sankhajith <isankhajith@gmail.com>
2023-12-11 21:58:48 +0530

    modified code

--- Leetcode_questions/Leetcode132.java
+++ Leetcode_questions/Leetcode132.java
@@ -66,3 +66,45 @@ class Solution {
         return minCuts[n - 1];
     }
 }
+
+
+class PalindromePartitioning {
+
+    public int minCut(String s) {
+        int n = s.length();
+
+        // Initialize a boolean table to check if a substring is a palindrome
+        boolean[][] isPalindrome = new boolean[n][n];
+
+        // Initialize an array to store the minimum cuts for each substring
+        int[] minCuts = new int[n];
+
+        // Calculate palindrome information
+        for (int end = 0; end < n; end++) {
+            minCuts[end] = end; // Maximum cuts needed for a substring of length end
+
+            for (int start = 0; start <= end; start++) {
+                // Check if the characters at start and end positions match
+                // and if the inner substring is also a palindrome
+                if (s.charAt(start) == s.charAt(end) && (end - start <= 1 || isPalindrome[start + 1][end - 1])) {
+                    isPalindrome[start][end] = true;
+
+                    // Update minimum cuts
+                    // If start is 0, it means the whole substring is a palindrome,
+                    // so no cut is needed; otherwise, update based on the previous cut
+                    minCuts[end] = (start == 0) ? 0 : Math.min(minCuts[end], minCuts[start - 1] + 1);
+                }
+            }
+        }
+
+        return minCuts[n - 1];
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        PalindromePartitioning solution = new PalindromePartitioning();
+
+        // Example usage:
+        System.out.println(solution.minCut("aab")); // Output: 1
+        System.out.println(solution.min
