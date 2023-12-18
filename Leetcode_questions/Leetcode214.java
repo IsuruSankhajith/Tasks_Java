5c28c229be47f39c2adfdedce5af98524c91e609
Isuru sankhajith <isankhajith@gmail.com>
2023-12-18 21:00:55 +0530

    modified code

--- Leetcode_questions/Leetcode214.java
+++ Leetcode_questions/Leetcode214.java
@@ -28,28 +28,47 @@ Constraints:
 0 <= s.length <= 5 * 104
 s consists of lowercase English letters only.
  */
-class Solution {
+public class ShortestPalindromeFinder {
+
     public String shortestPalindrome(String s) {
         String rev = new StringBuilder(s).reverse().toString();
         String combined = s + "#" + rev;
-        
-        int[] lps = new int[combined.length()];
-        // Construct the longest prefix suffix array
-        for (int i = 1; i < combined.length(); i++) {
+
+        int[] lps = computeLPS(combined);
+
+        int palindromeLength = lps[lps.length - 1];
+        String prefix = rev.substring(0, rev.length() - palindromeLength);
+
+        return prefix + s;
+    }
+
+    private int[] computeLPS(String str) {
+        int[] lps = new int[str.length()];
+        for (int i = 1; i < str.length(); i++) {
             int j = lps[i - 1];
-            while (j > 0 && combined.charAt(i) != combined.charAt(j)) {
+            while (j > 0 && str.charAt(i) != str.charAt(j)) {
                 j = lps[j - 1];
             }
-            if (combined.charAt(i) == combined.charAt(j)) {
+            if (str.charAt(i) == str.charAt(j)) {
                 lps[i] = j + 1;
             }
         }
-        
-        // The length of the palindrome that starts from the beginning
-        int palindromeLength = lps[lps.length - 1];
-        // Construct the palindrome by adding characters from the reversed string
-        String prefix = rev.substring(0, rev.length() - palindromeLength);
-        
-        return prefix + s;
+        return lps;
+    }
+
+    public static void main(String[] args) {
+        ShortestPalindromeFinder finder = new ShortestPalindromeFinder();
+
+        // Example 1
+        String s1 = "aacecaaa";
+        String result1 = finder.shortestPalindrome(s1);
+        System.out.println("Input: " + s1);
+        System.out.println("Output: " + result1);
+
+        // Example 2
+        String s2 = "abcd";
+        String result2 = finder.shortestPalindrome(s2);
+        System.out.println("Input: " + s2);
+        System.out.println("Output: " + result2);
     }
 }
