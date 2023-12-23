463c6b7c3b7dbed1f5f05add2dbae9504c1d41b1
Isuru sankhajith <isankhajith@gmail.com>
2023-12-23 19:44:31 +0530

    modified code

--- Leetcode_questions/Leetcode661 - Copy.java
+++ Leetcode_questions/Leetcode661 - Copy.java
@@ -76,3 +76,46 @@ class Solution {
         return count;
     }
 }
+////////////////////////////
+
+class DigitCounter {
+    public int countDigitOne(int n) {
+        int count = 0;
+        long factor = 1; // Initialize factor to 1
+
+        // Iterate through each digit place (ones, tens, hundreds, etc.)
+        while (n / factor > 0) {
+            long currentDigit = (n / factor) % 10; // Extract the current digit
+            long before = n / (factor * 10); // Part of the number before the current digit
+            long after = n % factor; // Part of the number after the current digit
+
+            if (currentDigit == 0) {
+                // If the current digit is 0, count the occurrences based on the number before the current digit
+                count += before * factor;
+            } else if (currentDigit == 1) {
+                // If the current digit is 1, count the occurrences based on both before and after the current digit
+                count += before * factor + after + 1;
+            } else {
+                // If the current digit is greater than 1, count the occurrences based on the number before the current digit plus one
+                count += (before + 1) * factor;
+            }
+
+            factor *= 10; // Move to the next digit place
+        }
+
+        return count;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        DigitCounter digitCounter = new DigitCounter();
+        
+        // Example usage
+        int result1 = digitCounter.countDigitOne(13);
+        System.out.println("Output 1: " + result1); // Output: 6
+
+        int result2 = digitCounter.countDigitOne(0);
+        System.out.println("Output 2: " + result2); // Output: 0
+    }
+}
