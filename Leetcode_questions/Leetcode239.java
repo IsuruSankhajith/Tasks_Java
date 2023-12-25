605892e6c0f77d39d4a30f0c2d86fcc9e84bf46d
Isuru sankhajith <isankhajith@gmail.com>
2023-12-25 19:59:04 +0530

    modified code

--- Leetcode_questions/Leetcode239.java
+++ Leetcode_questions/Leetcode239.java
@@ -70,3 +70,66 @@ class Solution {
         return result;
     }
 }
+////////////////
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+class MaxSlidingWindow {
+
+    private int[] nums;
+    private int k;
+    private int[] result;
+
+    public MaxSlidingWindow(int[] nums, int k) {
+        this.nums = nums;
+        this.k = k;
+        this.result = new int[nums.length - k + 1];
+    }
+
+    public int[] maxSlidingWindow() {
+        if (nums == null || nums.length == 0) {
+            return new int[0];
+        }
+
+        int n = nums.length;
+        int ri = 0;
+
+        Deque<Integer> deque = new ArrayDeque<>();
+
+        for (int i = 0; i < n; i++) {
+            // Remove elements outside of the current window
+            while (!deque.isEmpty() && deque.peek() < i - k + 1) {
+                deque.poll();
+            }
+
+            // Remove smaller elements as they won't affect the result
+            while (!deque.isEmpty() && nums[deque.peekLast()] < nums[i]) {
+                deque.pollLast();
+            }
+
+            // Add the current element to the deque
+            deque.offer(i);
+
+            // Add the maximum element to the result array for the current window
+            if (i >= k - 1) {
+                result[ri++] = nums[deque.peek()];
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {1, 3, -1, -3, 5, 3, 6, 7};
+        int k1 = 3;
+        MaxSlidingWindow solution1 = new MaxSlidingWindow(nums1, k1);
+        int[] result1 = solution1.maxSlidingWindow();
+        System.out.println(Arrays.toString(result1));
+
+        int[] nums2 = {1};
+        int k2 = 1;
+        MaxSlidingWindow solution2 = new MaxSlidingWindow(nums2, k2);
+        int[] result2 = solution2.maxSlidingWindow();
+        System.out.println(Arrays.toString(result2));
+    }
+}
