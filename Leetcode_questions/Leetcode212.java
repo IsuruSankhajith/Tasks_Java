020326a4096014439611995b324f7c64552f2814
Isuru sankhajith <isankhajith@gmail.com>
2023-12-27 23:15:56 +0530

    modified code

--- Leetcode_questions/Leetcode212.java
+++ Leetcode_questions/Leetcode212.java
@@ -119,3 +119,103 @@ public class Solution {
         System.out.println(solution.findWords(board2, words2)); // Output: []
     }
 }
+////////////////
+
+import java.util.ArrayList;
+import java.util.List;
+
+class TrieNode {
+    TrieNode[] children;
+    String word;
+
+    public TrieNode() {
+        children = new TrieNode[26];
+        word = null;
+    }
+}
+
+class WordSearch {
+    private char[][] board;
+    private TrieNode root;
+
+    public WordSearch(char[][] board, String[] words) {
+        this.board = board;
+        this.root = buildTrie(words);
+    }
+
+    public List<String> findWords() {
+        List<String> result = new ArrayList<>();
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                dfs(i, j, root, result);
+            }
+        }
+
+        return result;
+    }
+
+    private TrieNode buildTrie(String[] words) {
+        TrieNode root = new TrieNode();
+
+        for (String word : words) {
+            TrieNode node = root;
+            for (char ch : word.toCharArray()) {
+                int index = ch - 'a';
+                if (node.children[index] == null) {
+                    node.children[index] = new TrieNode();
+                }
+                node = node.children[index];
+            }
+            node.word = word;
+        }
+
+        return root;
+    }
+
+    private void dfs(int i, int j, TrieNode node, List<String> result) {
+        char ch = board[i][j];
+        if (ch == '#' || node.children[ch - 'a'] == null) {
+            return;
+        }
+
+        node = node.children[ch - 'a'];
+        if (node.word != null) {
+            result.add(node.word);
+            node.word = null; // Mark the word as found to avoid duplicates
+        }
+
+        board[i][j] = '#'; // Mark the cell as visited
+
+        int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        for (int[] dir : directions) {
+            int newRow = i + dir[0];
+            int newCol = j + dir[1];
+            if (newRow >= 0 && newRow < board.length && newCol >= 0 && newCol < board[0].length) {
+                dfs(newRow, newCol, node, result);
+            }
+        }
+
+        board[i][j] = ch; // Restore the original value of the cell
+    }
+
+    public static void main(String[] args) {
+        char[][] board1 = {
+                {'o', 'a', 'a', 'n'},
+                {'e', 't', 'a', 'e'},
+                {'i', 'h', 'k', 'r'},
+                {'i', 'f', 'l', 'v'}
+        };
+        String[] words1 = {"oath", "pea", "eat", "rain"};
+        WordSearch wordSearch1 = new WordSearch(board1, words1);
+        System.out.println(wordSearch1.findWords()); // Output: [eat, oath]
+
+        char[][] board2 = {
+                {'a', 'b'},
+                {'c', 'd'}
+        };
+        String[] words2 = {"abcb"};
+        WordSearch wordSearch2 = new WordSearch(board2, words2);
+        System.out.println(wordSearch2.findWords()); // Output: []
+    }
+}
