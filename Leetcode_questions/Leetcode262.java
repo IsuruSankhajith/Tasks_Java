ae104853c187e9e9675b38216da05d266789d74c
Isuru sankhajith <isankhajith@gmail.com>
2023-12-29 21:20:40 +0530

    modified code

--- Leetcode_questions/Leetcode262.java
+++ Leetcode_questions/Leetcode262.java
@@ -105,3 +105,13 @@ JOIN Users AS clients ON Trips.client_id = clients.users_id AND clients.banned =
 JOIN Users AS drivers ON Trips.driver_id = drivers.users_id AND drivers.banned = 'No'
 WHERE request_at BETWEEN '2013-10-01' AND '2013-10-03'
 GROUP BY request_at;
+//////////////////////////////////////////////
+
+SELECT request_at AS Day,
+       ROUND(SUM(CASE WHEN status = 'cancelled_by_driver' OR status = 'cancelled_by_client' THEN 1 ELSE 0 END)
+             / COUNT(DISTINCT Trips.id), 2) AS "Cancellation Rate"
+FROM Trips
+JOIN Users AS clients ON Trips.client_id = clients.users_id AND clients.banned = 'No'
+JOIN Users AS drivers ON Trips.driver_id = drivers.users_id AND drivers.banned = 'No'
+WHERE request_at BETWEEN '2013-10-01' AND '2013-10-03'
+GROUP BY request_at;
