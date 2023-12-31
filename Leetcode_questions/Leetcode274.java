33de0891ea8dd86d877e842221e249ff135dbaf0
Isuru sankhajith <isankhajith@gmail.com>
2023-12-31 14:18:28 +0530

    modified code

--- Leetcode_questions/Leetcode274.java
+++ Leetcode_questions/Leetcode274.java
@@ -28,31 +28,38 @@ Constraints:
 
 0 <= num <= 231 -  */
 class Solution {
-    public int[][] imageSmoother(int[][] img) {
-        int m = img.length;
-        int n = img[0].length;
-        int[][] result = new int[m][n];
-        
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                result[i][j] = getSmoothedValue(img, i, j, m, n);
-            }
+    private static final String[] THOUSANDS = {"", "Thousand", "Million", "Billion"};
+    private static final String[] BELOW_20 = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+    private static final String[] TENS = {"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+
+    public String numberToWords(int num) {
+        if (num == 0) {
+            return "Zero";
         }
-        
-        return result;
+
+        int i = 0;
+        String words = "";
+
+        while (num > 0) {
+            if (num % 1000 != 0) {
+                words = helper(num % 1000) + THOUSANDS[i] + " " + words;
+            }
+            num /= 1000;
+            i++;
+        }
+
+        return words.trim();
     }
-    
-    private int getSmoothedValue(int[][] img, int i, int j, int m, int n) {
-        int sum = 0;
-        int count = 0;
-        
-        for (int x = Math.max(0, i - 1); x <= Math.min(m - 1, i + 1); x++) {
-            for (int y = Math.max(0, j - 1); y <= Math.min(n - 1, j + 1); y++) {
-                sum += img[x][y];
-                count++;
-            }
+
+    private String helper(int num) {
+        if (num == 0) {
+            return "";
+        } else if (num < 20) {
+            return BELOW_20[num] + " ";
+        } else if (num < 100) {
+            return TENS[num / 10] + " " + helper(num % 10);
+        } else {
+            return BELOW_20[num / 100] + " Hundred " + helper(num % 100);
         }
-        
-        return sum / count;
     }
 }
