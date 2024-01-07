0fa30a9803a7dbbeff23552b2693185f5d96efee
Isuru sankhajith <isankhajith@gmail.com>
2024-01-07 22:25:47 +0530

    modified code

---
+++ Leetcode_questions/Leetcode298.java
@@ -0,0 +1,90 @@
+
+/*
+The median is the middle value in an ordered integer list. If the size of the list is even, there is no middle value, and the median is the mean of the two middle values.
+
+For example, for arr = [2,3,4], the median is 3.
+For example, for arr = [2,3], the median is (2 + 3) / 2 = 2.5.
+Implement the MedianFinder class:
+
+MedianFinder() initializes the MedianFinder object.
+void addNum(int num) adds the integer num from the data stream to the data structure.
+double findMedian() returns the median of all elements so far. Answers within 10-5 of the actual answer will be accepted.
+ 
+
+Example 1:
+
+Input
+["MedianFinder", "addNum", "addNum", "findMedian", "addNum", "findMedian"]
+[[], [1], [2], [], [3], []]
+Output
+[null, null, null, 1.5, null, 2.0]
+
+Explanation
+MedianFinder medianFinder = new MedianFinder();
+medianFinder.addNum(1);    // arr = [1]
+medianFinder.addNum(2);    // arr = [1, 2]
+medianFinder.findMedian(); // return 1.5 (i.e., (1 + 2) / 2)
+medianFinder.addNum(3);    // arr[1, 2, 3]
+medianFinder.findMedian(); // return 2.0
+ 
+
+Constraints:
+
+-105 <= num <= 105
+There will be at least one element in the data structure before calling findMedian.
+At most 5 * 104 calls will be made to addNum and findMedian.
+ 
+
+Follow up:
+
+If all integer numbers from the stream are in the range [0, 100], how would you optimize your solution?
+If 99% of all integer numbers from the stream are in the range [0, 100], how would you optimize your solution?class MedianFinder {
+
+    public MedianFinder() {
+        
+    }
+    
+    public void addNum(int num) {
+        
+    }
+    
+    public double findMedian() {
+        
+    }
+}
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */ */
+class Solution {
+    public int[][] imageSmoother(int[][] img) {
+        int m = img.length;
+        int n = img[0].length;
+        int[][] result = new int[m][n];
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                result[i][j] = getSmoothedValue(img, i, j, m, n);
+            }
+        }
+        
+        return result;
+    }
+    
+    private int getSmoothedValue(int[][] img, int i, int j, int m, int n) {
+        int sum = 0;
+        int count = 0;
+        
+        for (int x = Math.max(0, i - 1); x <= Math.min(m - 1, i + 1); x++) {
+            for (int y = Math.max(0, j - 1); y <= Math.min(n - 1, j + 1); y++) {
+                sum += img[x][y];
+                count++;
+            }
+        }
+        
+        return sum / count;
+    }
+}
