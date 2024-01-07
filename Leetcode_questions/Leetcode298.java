8e57423aeb669b97dd19e617785d4115b342259c
Isuru sankhajith <isankhajith@gmail.com>
2024-01-07 22:27:47 +0530

    modified code

--- Leetcode_questions/Leetcode298.java
+++ Leetcode_questions/Leetcode298.java
@@ -108,3 +108,54 @@ public class Main {
         System.out.println(medianFinder.findMedian()); // Output: 2.0
     }
 }
+//////////////////////////////////////////////
+
+import java.util.PriorityQueue;
+
+class MedianFinder {
+
+    private PriorityQueue<Integer> maxHeap; // Stores the smaller half of the numbers
+    private PriorityQueue<Integer> minHeap; // Stores the larger half of the numbers
+
+    public MedianFinder() {
+        maxHeap = new PriorityQueue<>((a, b) -> b - a);
+        minHeap = new PriorityQueue<>();
+    }
+
+    public void addNum(int num) {
+        // Add the number to the appropriate heap
+        if (maxHeap.isEmpty() || num <= maxHeap.peek()) {
+            maxHeap.offer(num);
+        } else {
+            minHeap.offer(num);
+        }
+
+        // Balance the heaps to ensure their sizes differ by at most 1
+        if (maxHeap.size() > minHeap.size() + 1) {
+            minHeap.offer(maxHeap.poll());
+        } else if (minHeap.size() > maxHeap.size()) {
+            maxHeap.offer(minHeap.poll());
+        }
+    }
+
+    public double findMedian() {
+        // If the heaps have the same size, take the average of their tops
+        if (maxHeap.size() == minHeap.size()) {
+            return (maxHeap.peek() + minHeap.peek()) / 2.0;
+        } else {
+            // Otherwise, the max heap contains the middle element
+            return maxHeap.peek();
+        }
+    }
+}
+
+public class MedianFinderExample {
+    public static void main(String[] args) {
+        MedianFinder medianFinder = new MedianFinder();
+        medianFinder.addNum(1);
+        medianFinder.addNum(2);
+        System.out.println("Median: " + medianFinder.findMedian()); // Output: 1.5
+        medianFinder.addNum(3);
+        System.out.println("Median: " + medianFinder.findMedian()); // Output: 2.0
+    }
+}
